b7d8ea551690e9360635dd965b08f1cd6a8fae52
Shivam gaba <shivamgabagaba@gmail.com>
2021-08-05 17:05:11 +0530

    Links Crawled

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from flask import Flask, render_template, request, redirect
-
+from bs4 import BeautifulSoup
+import requests
+import re
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
+pages = set()
+url_received = ""
 
 
-@app.route('/',  methods=["POST", "GET"])
+@app.route('/', methods=["POST", "GET"])
 def index():
     if request.method == 'POST':
-        url_received= request.form.get("url")
+        global url_received
+        url_received = request.form.get("url")
         print(url_received)
-    return render_template("index.html", title = "verification")
+
+        pattern = re.compile("^(/)")
+        html = requests.get(url_received).text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        for link in soup.find_all("a", href=pattern):
+            if "href" in link.attrs:
+                if link.attrs["href"] not in pages:
+                    new_page = link.attrs["href"]
+                    print(url_received+new_page)
+                    pages.add(url_received+new_page)
+
+    return render_template("index.html", title="verification")
+
 
 if __name__ == '__main__':
     app.run(port=5000)
